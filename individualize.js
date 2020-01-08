d36eaf8ddeec1e775042093460c96a6e5866dcd4
Lenard Frommelt <lenny09918050@googlemail.com>
2020-01-09 00:38:29 +0100

    first version of the recipe to create a page-head

--- individualize.js
+++ individualize.js
@@ -1,14 +1,2 @@
 
-const pathModule = require("path")
-const fs = require("fs")
-
-const packagePath = pathModule.resolve(process.cwd(), "ressources", "package.json")
-const package = require(packagePath)
-const thingyPath = pathModule.resolve(process.cwd(), "..")
-const name = pathModule.basename(thingyPath)
-package.name = name
-package.bin = {}
-package.bin[name] = "./index.js"
-packageString = JSON.stringify(package, null, 2)
-fs.writeFileSync(packagePath, packageString)
-console.log("individualized clisources!") 
+console.log("TODO: individualize your page-head = not implemented yet!") 
